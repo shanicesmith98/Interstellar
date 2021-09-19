036dbd0eced366948a6f53475d72e66012f46ee3
shanice s <shanices@shanices-MacBook-Pro.local>
2021-09-19 17:27:57 -0400

    removed audio files and added game physics

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -26,10 +33,41 @@ function preload ()
 function create() {
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
     this.add.image(400, 300, 'star');
-    this.add.image(100, 500, 'dude');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
     this.add.text(300, 10, "Interstellar", { font: "40px Times New Roman", fill: "#ffffff"});
-  
     this.add.text(340, 60, "by Shanice", { font: "20px Times New Roman", fill: "#ffffff"});
 }
 
